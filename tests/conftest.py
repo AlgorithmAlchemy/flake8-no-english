fae141869320ebb21be13bfe990cdfbe9bb73643
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-09-30 01:00:06 +0300

    test: add conftest.py fixture for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# tests/conftest.py
+import pytest
+from flake8_no_english.checker import NonEnglishChecker
+
+@pytest.fixture(autouse=True)
+def reset_flags():
+    NonEnglishChecker.nle_comments = True
+    NonEnglishChecker.nle_strings = False
+    NonEnglishChecker.nle001_enabled = True
+    NonEnglishChecker.nle002_enabled = True
